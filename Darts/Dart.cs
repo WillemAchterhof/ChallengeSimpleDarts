529d93862c2969a6588a6c97ba8473c8ba6b16ff
WillemAchterhof <willem.achterhof@hotmail.com>
2021-03-04 21:39:27 +0900

    Add project files.

---
+++ Darts/Dart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Darts
+{
+	public class Dart
+	{
+		public Dart(Random random)
+		{
+		}
+		public void Trow()
+		{
+
+		}
+	}
+}
